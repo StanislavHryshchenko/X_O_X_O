81e19d03ea9b9d407e447ff6afb8218cffe92384
StanislavHryshchenko <80348332+StanislavHryshchenko@users.noreply.github.com>
2021-03-17 18:10:25 +0200

    Add files via upload

---
+++ X_O/X_O/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace X_O
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(950, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(950, 59);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 36);
+            this.buttonPlay.TabIndex = 10;
+            this.buttonPlay.Text = "Restart";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 861);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPlay;
+    }
+}
+
---
+++ X_O/X_O/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O
+{
+    public partial class Form1 : Form
+    {
+        private int x = 12, y = 12;
+        private Button[,] buttons = new Button[3,3];
+        private int player;
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Width = 900;
+            this.Size = new Size(800, 700);
+            player = 1;
+            label1.Text = "Player № 1";
+            for(int i = 0; i < buttons.Length/3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i,j] = new Button();
+                    buttons[i, j].Size = new Size(200, 200);
+                }
+            }
+            setButtons();
+        }
+
+        private void setButtons()
+        {
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j].Location = new Point(12 + 206 * j, 12 + 206 * i);
+                    buttons[i, j].Click += button1_Click;
+                    buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 72);
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (player)
+            {
+                case 1:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "x");
+
+                    player = 0;
+                    label1.Text = "Player № 2";
+                    break;
+                case 0:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "o");
+                    player = 1;
+                    label1.Text = "Player № 1";
+                    break;
+            }
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            checkwin();
+        }
+        private void checkwin()
+        {
+            if (buttons[0,0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+            {
+                if (buttons[1, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+
+            }
+            if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+            {
+                if (buttons[0, 1].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 2].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+            if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show("You win!");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+    }
+}
